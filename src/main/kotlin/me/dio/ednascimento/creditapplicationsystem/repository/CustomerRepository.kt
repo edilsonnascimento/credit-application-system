b8bfb5b60ef47594dd26fff51800f83b36330b91
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-20 08:14:57 -0300

    09 - REFACTORED directory

--- src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/repository/CustomerRepository.kt
+++ src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/repository/CustomerRepository.kt
@@ -1,4 +1,4 @@
-package me.dio.ednascimento.creditapplicationsystem.repositoy
+package me.dio.ednascimento.creditapplicationsystem.repository
 
 import me.dio.ednascimento.creditapplicationsystem.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
