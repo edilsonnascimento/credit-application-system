bd1b2ef18977e351ec32500dc5317575f60b30e9
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-20 16:10:27 -0300

    11 - ADD service layer interfaces.

---
+++ src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/service/ICreditService.kt
@@ -0,0 +1,10 @@
+package me.dio.ednascimento.creditapplicationsystem.service
+
+import me.dio.ednascimento.creditapplicationsystem.entity.Credit
+import java.util.*
+
+interface ICreditService {
+    fun save(credit: Credit) : Credit
+    fun findAllByCustomers(customerId: Long): List<Credit>
+    fun findByCreditCode(creditCode: UUID): Credit
+}
