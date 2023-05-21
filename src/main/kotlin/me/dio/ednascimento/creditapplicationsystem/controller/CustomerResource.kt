ff7d1528df7017b82c47791c0a1b68b7b8d0156b
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-21 05:35:14 -0300

    15 - ADD endPoint findById Customer

--- src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/controller/CustomerResource.kt
@@ -1,11 +1,8 @@
 package me.dio.ednascimento.creditapplicationsystem.controller
 
-import me.dio.ednascimento.creditapplicationsystem.dto.CustomerDto
+import me.dio.ednascimento.creditapplicationsystem.dto.*
 import me.dio.ednascimento.creditapplicationsystem.service.impl.CustomerService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/customers")
@@ -14,8 +11,14 @@ class CustomerResource(
 ) {
 
     @PostMapping
-    fun saveCustomer(@RequestBody customerDto: CustomerDto) : String {
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
         val saveCustomer = customerService.save(customerDto.toEntity())
         return "Custormer ${saveCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer = customerService.findById(id)
+        return CustomerView(customer)
     }
 }
