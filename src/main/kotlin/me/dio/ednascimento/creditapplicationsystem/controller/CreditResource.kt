11f3ad49720a1b04ca81df901fc9de892d34a480
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-21 06:32:56 -0300

    19 - ADD endPoint find credits customer

--- src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/controller/CreditResource.kt
@@ -1,8 +1,9 @@
 package me.dio.ednascimento.creditapplicationsystem.controller
 
-import me.dio.ednascimento.creditapplicationsystem.dto.CreditDto
+import me.dio.ednascimento.creditapplicationsystem.dto.*
 import me.dio.ednascimento.creditapplicationsystem.service.impl.CreditService
 import org.springframework.web.bind.annotation.*
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -15,4 +16,10 @@ class CreditResource(
         val credit = creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.lastName} saved"
     }
+
+    @GetMapping
+    fun findAllByCustomerId(@RequestParam customerId: Long) = creditService
+            .findAllByCustomers(customerId).stream()
+            .map { credit -> CreditViewList(credit) }
+            .collect(Collectors.toList())
 }
