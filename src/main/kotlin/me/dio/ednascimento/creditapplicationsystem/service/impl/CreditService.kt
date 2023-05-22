c0e48b29c9fbbd741ded5fd95882423de869807e
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-22 16:38:31 -0300

    30 - ADD spring security dependecy.

--- src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/service/impl/CreditService.kt
@@ -26,7 +26,6 @@ class CreditService(
 
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
         val credit: Credit = creditRepository.findByCreditCode(creditCode)
-            ?: throw BusinessException("CreditCode $creditCode not found")
         return if(credit.customer?.id == customerId) credit else throw IllegalArgumentException("Contact admin")
     }
 }
