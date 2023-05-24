6923267fab4965e1391db6fac3a3646e12223932
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-24 04:31:00 -0300

    36 - ADD class configuration openApi to uses swagger.

---
+++ src/main/kotlin/me/dio/ednascimento/creditapplicationsystem/config/Swagger3Config.kt
@@ -0,0 +1,17 @@
+package me.dio.ednascimento.creditapplicationsystem.config
+
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Swagger3Config {
+
+    @Bean
+    fun publicApi(): GroupedOpenApi? {
+        return GroupedOpenApi.builder()
+            .group("springcreditapplicationsystem-public")
+            .pathsToMatch("/api/customers/**", "/api/credits/**")
+            .build()
+    }
+}
